158d74147e5850bda8c3021558a458a6abdeb211
OmarMahmoudElsherif <104269847+OmarMahmoudElsherif@users.noreply.github.com>
2022-11-08 00:57:05 +0200

    Add files via upload

---
+++ Eclipse_wk/Control_ECU/Debug/Control_Ecu.d
@@ -0,0 +1,21 @@
+Control_Ecu.o Control_Ecu.o: ../Control_Ecu.c ../std_types.h ../uart.h \
+  ../external_eeprom.h ../twi.h ../dc_motor.h ../gpio.h ../PWM_Timer0.h \
+  ../timer1.h ../buzzer.h
+
+../std_types.h:
+
+../uart.h:
+
+../external_eeprom.h:
+
+../twi.h:
+
+../dc_motor.h:
+
+../gpio.h:
+
+../PWM_Timer0.h:
+
+../timer1.h:
+
+../buzzer.h:
