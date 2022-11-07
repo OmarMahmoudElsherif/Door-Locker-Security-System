4741f6f01e7529d802e9f6c7116232072c54ad2d
OmarMahmoudElsherif <104269847+OmarMahmoudElsherif@users.noreply.github.com>
2022-11-08 00:54:44 +0200

    Add files via upload

---
+++ Eclipse_wk/HMI_ECU/Debug/Human_Machine_Interface.d
@@ -0,0 +1,13 @@
+Human_Machine_Interface.o Human_Machine_Interface.o:  \
+ ../Human_Machine_Interface.c ../std_types.h ../lcd.h ../keypad.h \
+  ../uart.h ../timer1.h
+
+../std_types.h:
+
+../lcd.h:
+
+../keypad.h:
+
+../uart.h:
+
+../timer1.h:
